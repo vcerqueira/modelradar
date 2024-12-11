a8b1565eeda53377d84bb7fd6ac78a782abac7ee
Vitor Cerqueira <cerqueira.vitormanuel@gmail.com>
2024-12-11 23:07:56 +0000

    plotter

--- modelradar/visuals/plotter.py
+++ modelradar/visuals/plotter.py
@@ -1,61 +1,11 @@
-import pandas as pd
-
-from utilsforecast.losses import smape, mape
-
-from modelradar.evaluate.radar import ModelRadar
-
-cv = pd.read_csv('assets/cv.csv')
-
-radar = ModelRadar(cv_df=cv,
-                   freq='ME',
-                   metrics=[smape, mape],
-                   model_names=None,
-                   hardness_reference='SeasonalNaive',
-                   ratios_reference='NHITS',
-                   rope=10)
-
-err = radar.evaluate(keep_uids=True)
-err_hard = radar.uid_accuracy.get_hard_uids(err)
-# cv_hard = cv.query(f'unique_id == {radar.uid_accuracy.hard_uid}')
-
-win_ratios_df = radar.rope.get_winning_ratios(err, return_long=True)
-radar.rope.get_winning_ratios(err)
-
-radar.rope.get_winning_ratios(err_hard)
-
-radar.uid_accuracy.expected_shortfall(err)
-radar.uid_accuracy.expected_shortfall(err, return_df=True)
-radar.uid_accuracy.expected_shortfall(err_hard)
-
-eval_overall = radar.evaluate()
-# eval_overall = radar.evaluate(return_df=True)
-# radar.evaluate(cv=cv_hard.reset_index())
-eval_hbounds = radar.evaluate_by_horizon_bounds(return_long=True)
-
-eval_fhorizon = radar.evaluate_by_horizon()
-eval_fhorizon.melt('horizon')
-
-radar.evaluate_by_anomaly(anomaly_col='is_anomaly', mode='observations')
-radar.evaluate_by_anomaly(anomaly_col='is_anomaly', mode='series')
-error_on_group = radar.evaluate_by_group(group_col='is_anomaly')
-
-# distribution of errors
-
-#
 from typing import List
+
 import numpy as np
+import pandas as pd
 import plotnine as p9
+
 from modelradar.visuals.config import THEME
 from modelradar.pipelines.utils import LogTransformation
-
-plot = ModelRadarPlotter.error_barplot(data=eval_overall, x='Model', y='Performance')
-plot = ModelRadarPlotter.error_by_horizon_fl(data=eval_hbounds, model_cats=radar.model_order)
-plot = ModelRadarPlotter.error_by_horizon(data=eval_fhorizon)
-plot = ModelRadarPlotter.winning_ratios(data=win_ratios_df, reference=radar.rope.reference)
-plot = ModelRadarPlotter.error_by_group(data=error_on_group, model_cats=radar.model_order)
-plot = ModelRadarPlotter.error_distribution(data=err, model_cats=radar.model_order)
-
-plot.save('test.pdf')
 
 
 class ModelRadarPlotter:
@@ -197,4 +147,3 @@ class ModelRadarPlotter:
                p9.guides(fill=None)
 
         return plot
-
